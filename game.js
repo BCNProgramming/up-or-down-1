d1049c3bb3b49de45a76c63399d57cfb115ff9cd
rglepe <rglepe@gmail.com>
2019-02-07 21:11:55 +0100

    formato ok

--- game.js
+++ game.js
@@ -1,21 +1,19 @@
-'use strict';
+"use strict";
 
 class Game {
-
   constructor() {
     this.onGameOverCallback = null;
     this.score = 0;
     this.timeLeft = null;
 
     //   this.cards =;
-    this.cards=null;
+    this.cards = null;
     this.step = null;
     this.maxStep = 10;
-}
+  }
 
-start() {
-  
-  this.gameMain = buildDom(`
+  start() {
+    this.gameMain = buildDom(`
     <main class="game container">
       <header>
         <div class="score">
@@ -44,144 +42,131 @@ start() {
     </main>
   `);
 
-  this.scoreElement = this.gameMain.querySelector('.score .value');
-  this.timeLeftElement = this.gameMain.querySelector('.timer .value');
+    this.scoreElement = this.gameMain.querySelector(".score .value");
+    this.timeLeftElement = this.gameMain.querySelector(".timer .value");
 
-  this.currentCardElement = this.gameMain.querySelector('.current-card');
-  this.nextCardElement = this.gameMain.querySelector('.next-card');
+    this.currentCardElement = this.gameMain.querySelector(".current-card");
+    this.nextCardElement = this.gameMain.querySelector(".next-card");
 
-  this.buttonUp = this.gameMain.querySelector('.up');
-  this.buttonDown = this.gameMain.querySelector('.down');
+    this.buttonUp = this.gameMain.querySelector(".up");
+    this.buttonDown = this.gameMain.querySelector(".down");
 
-  this.stepNoElement = this.gameMain.querySelector('.step-no');
-  this.totalStepsElement = this.gameMain.querySelector('.total-steps');
+    this.stepNoElement = this.gameMain.querySelector(".step-no");
+    this.totalStepsElement = this.gameMain.querySelector(".total-steps");
 
-  document.body.appendChild(this.gameMain);
+    document.body.appendChild(this.gameMain);
 
-  this.showFirstCard();
-}
+    this.showFirstCard();
+  }
 
-getRandomCard(min, max) {
+  getRandomCard(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-}
+  }
 
+  showFirstCard() {
+    this.totalStepsElement.innerText = this.maxStep;
 
-showFirstCard() {
-  
-
-  this.totalStepsElement.innerText = this.maxStep;
-
-  this.step = 0;
-  this.showCard();
-  this.startTimer();
-}
-
-triggerTimeout() {
-  
-  this.score--;
-  this.scoreElement.innerText = this.score;
-  this.nextCard();
-}
-
-nextCard () {
-  
-  this.step++;
-
-  if (this.step === this.maxStep) {
-    this.onGameOverCallback();
-  } else {
+    this.step = 0;
     this.showCard();
     this.startTimer();
   }
-}
 
-showCard() {
-  
-  this.currentCard = this.getRandomCard(1,10);
-  this.currentCardElement.innerText = this.currentCard;
-  this.nextCardElement.innerText = '?';
-
-  this.stepNoElement.innerText = this.step + 1;
-
-  this.handleClickUp = function () {
-    this.revealNumber(true);
-  };
-  this.buttonUp.addEventListener('click', this.handleClickUp);
-  this.buttonUp.removeAttribute('disabled');
-
-  this.handleClickDown = function () {
-    this.revealNumber(false);
+  triggerTimeout() {
+    this.score--;
+    this.scoreElement.innerText = this.score;
+    this.nextCard();
   }
-  this.buttonDown.addEventListener('click', this.handleClickDown);
-  this.buttonDown.removeAttribute('disabled');
-}
 
-startTimer() {
+  nextCard() {
+    this.step++;
 
-  this.timeLeft = 3;
-  this.timeLeftElement.innerText = this.timeLeft;
-  this.intervalId = window.setInterval(function () {
-    this.timeLeft--;
-    this.timeLeftElement.innerText = this.timeLeft;
-
-    if (this.timeLeft === 0) {
-      clearInterval(this.intervalId);
-      this.triggerTimeout();
+    if (this.step === this.maxStep) {
+      this.onGameOverCallback();
+    } else {
+      this.showCard();
+      this.startTimer();
     }
-  }, 1000);
-}
+  }
+
+  showCard() {
+    this.currentCard = this.getRandomCard(1, 10);
+    this.currentCardElement.innerText = this.currentCard;
+    this.nextCardElement.innerText = "?";
+
+    this.stepNoElement.innerText = this.step + 1;
+
+    this.handleClickUp = function() {
+      this.revealNumber(true);
+    };
+    this.buttonUp.addEventListener("click", this.handleClickUp);
+    this.buttonUp.removeAttribute("disabled");
+
+    this.handleClickDown = function() {
+      this.revealNumber(false);
+    };
+    this.buttonDown.addEventListener("click", this.handleClickDown);
+    this.buttonDown.removeAttribute("disabled");
+  }
+
+  startTimer() {
+    this.timeLeft = 3;
+    this.timeLeftElement.innerText = this.timeLeft;
+    this.intervalId = window.setInterval(function() {
+      this.timeLeft--;
+      this.timeLeftElement.innerText = this.timeLeft;
+
+      if (this.timeLeft === 0) {
+        clearInterval(this.intervalId);
+        this.triggerTimeout();
+      }
+    }, 1000);
+  }
 
   revealNumber(answerWasUp) {
-  
-    
-  clearInterval(this.intervalId);
-  this.buttonUp.removeEventListener('click', this.handleClickUp);
-  this.buttonUp.setAttribute('disabled', 'disabled');
-  this.buttonDown.removeEventListener('click', this.handleClickDown);
-  this.buttonDown.setAttribute('disabled', 'disabled');
+    clearInterval(this.intervalId);
+    this.buttonUp.removeEventListener("click", this.handleClickUp);
+    this.buttonUp.setAttribute("disabled", "disabled");
+    this.buttonDown.removeEventListener("click", this.handleClickDown);
+    this.buttonDown.setAttribute("disabled", "disabled");
 
-  var currentCard = this.getRandomCard(1,10);
-  var nextCard = this.getRandomCard(1,10);
+    var currentCard = this.getRandomCard(1, 10);
+    var nextCard = this.getRandomCard(1, 10);
 
-  var className = '';
+    var className = "";
 
-  if (answerWasUp && nextCard > currentCard) {
-    this.score++;
-    className = 'correct';
-  } else if (answerWasUp && nextCard < currentCard) {
-    this.score--;
-    className = 'incorrect';
-  } else if (!answerWasUp && nextCard < currentCard) {
-    this.score++;
-    className = 'correct';
-  } else if (!answerWasUp && nextCard > currentCard) {
-    this.score--;
-    className = 'incorrect';
-  } else {
-    this.score--;
-    className = 'incorrect';
+    if (answerWasUp && nextCard > currentCard) {
+      this.score++;
+      className = "correct";
+    } else if (answerWasUp && nextCard < currentCard) {
+      this.score--;
+      className = "incorrect";
+    } else if (!answerWasUp && nextCard < currentCard) {
+      this.score++;
+      className = "correct";
+    } else if (!answerWasUp && nextCard > currentCard) {
+      this.score--;
+      className = "incorrect";
+    } else {
+      this.score--;
+      className = "incorrect";
+    }
+
+    this.scoreElement.innerText = this.score;
+
+    this.nextCardElement.classList.add(className);
+    this.nextCardElement.innerText = nextCard;
+
+    setTimeout(function() {
+      this.nextCardElement.classList.remove(className);
+      this.nextCard();
+    }, 2000);
   }
 
-  this.scoreElement.innerText = this.score;
+  onOver(callback) {
+    this.onGameOverCallback = callback;
+  }
 
-  this.nextCardElement.classList.add(className);
-  this.nextCardElement.innerText = nextCard;
-
-  setTimeout(function () {
-    this.nextCardElement.classList.remove(className);
-    this.nextCard();
-  }, 2000);
-}
-
-
-onOver(callback) {
-  
-  this.onGameOverCallback = callback;
-}
-
-destroy() {
-  
-  this.gameMain.remove();
-}
-
+  destroy() {
+    this.gameMain.remove();
+  }
 }
